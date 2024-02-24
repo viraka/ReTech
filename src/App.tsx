2bbb959a3ba3825c628f162626d719d1c509ecb0
BlazeSN860 <virajprabhup@gmail.com>
2024-02-24 11:39:48 +0530

    feat: Created Router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Toaster } from 'react-hot-toast'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import NotFound from './pages/NotFound/NotFound';
+import SignUp from './pages/SignUp/SignUp';
+import SignIn from './pages/SignIn/SignIn';
+import Home from './pages/Home/Home';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const router = createBrowserRouter([
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+    {
+      path: "/404",
+      element: <NotFound />,
+    },
+    {
+      path: "/signup",
+      element: <SignUp />,
+    },
+    {
+      path: "/signin",
+      element: <SignIn />,
+    },
+    {
+      path: "/",
+      element: <Home />,
+    }
+  ]);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router} />
+      <Toaster
+        position="bottom-center"
+        reverseOrder={false}
+        toastOptions={{
+          success: {
+            style: {
+              background: "var(--border)",
+              color: "var(--foreground)",
+            },
+          },
+          error: {
+            style: {
+              background: "var(--border)",
+              color: "var(--foreground)",
+            },
+          },
+        }}
+      />
     </>
   )
 }
