6d27666d9040bc3b3911b517e54cee359e80eadf
BlazeSN860 <virajprabhup@gmail.com>
2024-02-24 19:39:14 +0530

    fix: Fixed signup.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,12 +11,12 @@ const Navbar = () => {
                     <ul>
                         <li><Link to={'/'}>Home</Link></li>
                         <li><Link to={'#about'}>About</Link></li>
-                        <li><Link to={'/services'}>Services</Link></li>
+                        <li><Link to={'/search'}>Services</Link></li>
                     </ul>
                 </div>
                 <div className={styles.navbarButtons}>
                     <button onClick={() => navigate('/login')}>Log In</button>
-                    <button onClick={() => navigate('/signup')}><span><FaCirclePlus/></span> Add Shop</button>
+                    <button onClick={() => navigate('/signup')}><span><FaCirclePlus /></span> Add Shop</button>
                 </div>
             </div>
         </>
