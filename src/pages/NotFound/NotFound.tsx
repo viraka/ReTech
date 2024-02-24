8a3f2ca15035633b976ff57e3b532fdf71b1c9d3
BlazeSN860 <virajprabhup@gmail.com>
2024-02-24 17:32:21 +0530

    feat: added notfound.

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,10 @@
-
+import styles from './NotFound.module.css'
 const NotFound = () => {
     return (
-        <div>NotFound</div>
+        <div className={styles.NotFound}>
+            <h1>404 Not Found</h1>
+            <p>Sorry, the page you are looking for might be in another castle!</p>
+        </div>
     )
 }
 
