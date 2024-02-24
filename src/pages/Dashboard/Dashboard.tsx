dec00e133b257ec27491cb663e20054b497110c8
BlazeSN860 <virajprabhup@gmail.com>
2024-02-24 17:47:23 +0530

    feat: Added Dashboard.

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,16 @@
+import styles from './Dashboard.module.css'
+
+
+const Dashboard = () => {
+    return (
+        <>
+            <div className={styles.dashWrapper}>
+                <h1>Dashboard</h1>
+                <p>Welcome to your dashboard</p>
+
+            </div>
+        </>
+    )
+}
+
+export default Dashboard
