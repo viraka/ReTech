45e85c66ae00ef11d8ba3797d8562c36025da7ca
BlazeSN860 <virajprabhup@gmail.com>
2024-02-25 17:35:35 +0530

    chores: better comments.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,27 +1,27 @@
 import styles from './Home.module.css'
 import Navbar from '../../components/Navbar/Navbar'
 import image from '../../assets/Back.png'
-import { IoSearch } from 'react-icons/io5'
-import { useNavigate } from 'react-router-dom'
+// import { IoSearch } from 'react-icons/io5'
+// import { useNavigate } from 'react-router-dom'
 import LocationModal from '../../components/LocationModal/LocationModal.jsx'
-import { useState } from 'react'
+//import { useState } from 'react'
 const Home = () => {
 
-    const navigate = useNavigate();
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    // const navigate = useNavigate();
+    // const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const handleOpenModal = () => {
-        setIsModalOpen(true);
-    };
+    // const handleOpenModal = () => {
+    //     setIsModalOpen(true);
+    // };
 
-    const handleCloseModal = () => {
-        setIsModalOpen(false);
-    };
+    // const handleCloseModal = () => {
+    //     setIsModalOpen(false);
+    // };
 
-    const handleLocationSubmit = (location: string) => {
-        // Handle the location submission logic here
-        console.log('Location submitted:', location);
-    };
+    // const handleLocationSubmit = (location: string) => {
+    //     // Handle the location submission logic here
+    //     console.log('Location submitted:', location);
+    // };
 
     return (
         <>
