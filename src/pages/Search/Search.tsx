b4c9e1cfa7e9642520710bf377a5955754622ce7
BlazeSN860 <virajprabhup@gmail.com>
2024-02-25 15:07:03 +0530

    fix: Setup Search.

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,10 +1,52 @@
+import { useEffect, useState } from 'react'
 import styles from './Search.module.css'
-
+import Card from './components/Card/Card'
+import SearchBar from './components/SearchBar/SearchBar'
+import { supabase } from '../../utils/supabase'
 const Search = () => {
+    const [data, setData] = useState<shop[]>([])
+    const [selectedOption, setSelectedOption] = useState('product');
+    const [searchTerm, setSearchTerm] = useState('');
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data: res, error } = await supabase
+                .from('shop')
+                .select('*')
+            if (error) {
+                throw error
+            }
+
+            setData(res as shop[])
+            console.log(data)
+        }
+        fetchData()
+    }, [])
+
     return (
         <>
             <div className={styles.searchWrapper}></div>
-            <h1>Search</h1>
+            <SearchBar
+                selectedOption={selectedOption}
+                setSelectedOption={setSelectedOption}
+                searchTerm={searchTerm}
+                setSearchTerm={setSearchTerm}
+            />
+            {data?.filter((shop) => {
+                if (searchTerm === '') {
+                    return true;
+                }
+                return ((shop.services.some(service => service.startsWith(searchTerm)) && selectedOption == 'service')
+                    || (shop.products.some(product => product.startsWith(searchTerm)) && selectedOption == 'product'))
+            })
+                .map((shop, index) =>
+                (
+                    <Card
+                        key={index}
+                        shop={shop}
+                    />
+                )
+                )}
+
         </>
     )
 }
